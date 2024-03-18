d427ebc3c71e13344a46476b2308f1a490bfe0c6
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-03-18 20:23:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to("#box", {
+  x: 650,
+  y: 275,
+  duration: 3,
+  delay: 1,
+  backgroundColor: "dodgerblue",
+  rotate: 720,
+});
+
+gsap.to("#box-2", {
+  x: -700,
+  y: -215,
+  duration: 3,
+  delay: 1,
+  backgroundColor: "limegreen",
+  rotate: -720,
+});

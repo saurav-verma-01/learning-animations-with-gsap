a12721a5402d5cdf3e143883c82ee29de0e2ac09
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-03-18 23:13:24 +0530

    static design completed

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-gsap.to("#box", {
-  x: 650,
-  y: 275,
-  duration: 3,
-  delay: 1,
-  backgroundColor: "dodgerblue",
-  rotate: 720,
-});
-
-gsap.to("#box-2", {
-  x: -700,
-  y: -215,
-  duration: 3,
-  delay: 1,
-  backgroundColor: "limegreen",
-  rotate: -720,
-});
+ 
